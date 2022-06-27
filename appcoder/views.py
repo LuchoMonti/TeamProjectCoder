0ab6ebdc9fec8197f315d58c6cadc8373f527094
Willy Crook <lucho_monti@hotmail.com>
2022-06-27 10:42:15 -0300

    falta copiar diseño de web

--- appcoder/views.py
+++ appcoder/views.py
@@ -1,5 +1,9 @@
+from mailbox import NoSuchMailboxError
+from django import template
+from django.template import loader
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Prueba
 
 # Create your views here.
 
@@ -7,4 +11,18 @@ def una_vista(request):
     return HttpResponse('<h1>Titulo Pagina</h1>')
 
 def un_template(request):
-    return HttpResponse('<h1>Mi Template</h1>')
+    
+    # template = loader.get_template('index.html')
+    
+    prueba1 = Prueba(nombre='primero')
+    prueba2 = Prueba(nombre='segundo')
+    prueba3 = Prueba(nombre='tercero')
+    prueba1.save()
+    prueba2.save()
+    prueba3.save()
+    
+    # render = template.render({'lista_objeto': [prueba1, prueba2, prueba3]})
+    
+    return render(request, 'index.html', {'lista_objeto': [prueba1, prueba2, prueba3]})
+    
+    return HttpResponse(render)
